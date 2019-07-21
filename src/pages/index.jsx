b4c322a5784dce9a93285bebd002d36c9777c6f4
Sudhir Yadav <yadavsudhir405@gmail.com>
2019-07-21 11:45:41 +0530

    Build check

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,7 +3,7 @@ import Images from "../constants/images";
 
 const Index = () => (
   <div>
-    <h1>This is the index page</h1>
+    <h1>This is the index page Edited</h1>
     <img src={Images.LOGO} alt="logo" />
   </div>
 );
